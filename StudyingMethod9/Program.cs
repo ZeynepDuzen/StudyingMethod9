136c9ab64b94b8ba725511d9f161f3bbfe078f5f
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 07:52:43 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod9
+{
+    internal class Program
+    {
+        static int Sum(int x)
+        {
+            return x + 3;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(9));
+
+            Console.Read();
+        }
+    }
+}
---
+++ StudyingMethod9A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod9A
+{
+    internal class Program
+    {
+        static int Carp(int y)
+        {
+            return y * 5;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Carp(5));
+
+           
+        }
+    }
+}
